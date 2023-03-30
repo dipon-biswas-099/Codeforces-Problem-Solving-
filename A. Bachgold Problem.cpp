2354ac4cf63a92352565401319ce094d1816a726
dipon-biswas-099 <diponbiswas099@gmail.com>
2023-03-31 01:56:05 +0600

    Solution

---
+++ A. Bachgold Problem.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n%2==0)
+    {
+        int possible=n/2;
+        cout<<possible<<endl;
+        for(int i=0;i<n/2;i++)
+        {
+            cout<<"2"<<" ";
+        }
+    }
+
+    else
+    {   int ps= n/2;
+        cout<<ps<<"\n";
+        int possi = n-3;
+        for(int i=1;i<=possi/2;i++)
+        {
+            cout<<"2"<<" ";
+        }
+        cout<<"3";
+
+
+    }
+
+
+
+
+    return 0;
+}
+
